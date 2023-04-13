d633f208e7189e9e2376bfaa393ef91214eac448
이한솔 <hansol8701@naver.com>
2023-04-13 13:15:00 +0900

    refactor : split 리팩토링

--- src/main/java/javastudy/study/ch02/calculator/v2/StringCalculator.java
+++ src/main/java/javastudy/study/ch02/calculator/v2/StringCalculator.java
@@ -7,24 +7,24 @@ import java.util.regex.Pattern;
 public class StringCalculator {
     private final static String DELIMITER = ",|:";
     private final static String REGEX = "//(.)\n(.*)";
+
     public int add(String text) {
         if (text.isEmpty()) {
             return 0;
         }
-        Pattern pattern = Pattern.compile(REGEX);
-        Matcher matcher = pattern.matcher(text);
+        String[] tokens = split(text);
+        return Arrays.stream(tokens)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    private String[] split(String text) {
+        Matcher matcher = Pattern.compile(REGEX).matcher(text);
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
             String subText = matcher.group(2);
-            String[] tokens = subText.split(customDelimiter);
-            return Arrays.stream(tokens)
-                    .mapToInt(Integer::parseInt)
-                    .sum();
-        }else {
-            String[] tokens = text.split(DELIMITER);
-            return Arrays.stream(tokens)
-                    .mapToInt(Integer::parseInt)
-                    .sum();
+            return subText.split(customDelimiter);
         }
+        return text.split(DELIMITER);
     }
 }
