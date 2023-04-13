688a497668212534aeba6da7416a4a32d69d3681
이한솔 <hansol8701@naver.com>
2023-04-13 12:55:31 +0900

    refactor : 구분자를 상수 처리

--- src/main/java/javastudy/study/ch02/calculator/v2/StringCalculator.java
+++ src/main/java/javastudy/study/ch02/calculator/v2/StringCalculator.java
@@ -3,11 +3,12 @@ package javastudy.study.ch02.calculator.v2;
 import java.util.Arrays;
 
 public class StringCalculator {
+    private final static String DELIMITER = ",|:";
     public int add(String text) {
         if (text.isEmpty()) {
             return 0;
         }
-        String[] token = text.split(",|:");
+        String[] token = text.split(DELIMITER);
         return Arrays.stream(token)
                 .mapToInt(Integer::parseInt)
                 .sum();
